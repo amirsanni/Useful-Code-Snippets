d816afaf2185e49b54dc6f7afb3ed1b4f4fa3981
Amir <amirsanni@gmail.com>
2016-05-13 10:58:12 +0100

    Update random_string.php

--- PHP/random_string.php
+++ PHP/random_string.php
@@ -1,12 +1,11 @@
 /**
  * @description function to generate a variable-length random string with an optional delimiter in between
- * @param string $codeType string to pass as 2nd param to codeIgniter's random_string() e.g. alnum, numeric etc
  * @param int $minLength minimum length of string to generate
  * @param int $maxLength maximum length of string to generate
- * @param string $delimiter [optional] The string to put in between the first and second strings. Default is underscore
+ * @param string $delimiter [optional] The string to put in between the first and second strings. Default is underscore. Pass an empty str if you don't want a delimiter
  * @return string $code the new randomly generated code
  */
-function generateRandomCode($codeType, $minLength, $maxLength, $delimiter = "_"){
+function generateRandomCode($minLength, $maxLength, $delimiter = "_"){
   //randomly generate the final length of the string we want to generate, ensuring it's between the min and max length provided by the user
   $totLength = rand($minLength, $maxLength-1);//($maxLength - 1) is used in order to accommodate the delimiter without going beyond the maxLength
   
@@ -14,10 +13,15 @@ function generateRandomCode($codeType, $minLength, $maxLength, $delimiter = "_")
   $b4_ = rand(1, $totLength-1);//number of strings before the delimiter
   $afta_ = $totLength - $b4_;//number of strings after the delimiter
   
-  //let's generate two random strings using CI's random_string function and merge them together
-  //Ensure you autoload CI's string helper or load it here manually
-  //e.g. $this->load->helper("string");
-  $rand_str = random_string($codeType, $b4_) . $delimiter . random_string($codeType, $afta_);
+  //CI's random_string function
+  $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+  //generate the two strings
+  $rand_1 = substr(str_shuffle(str_repeat($pool, ceil($totLength / strlen($pool)))), 0, $b4_);
+  $rand_2 = substr(str_shuffle(str_repeat($pool, ceil($totLength / strlen($pool)))), 0, $afta_);
+  
+  //merge the strings separating them with the delimiter
+  $rand_str = $rand_1 . $delimiter . $rand_2;
   
   return $rand_str;
 }
